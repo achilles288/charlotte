093bc24d6525181a969ea31cabf31adc4eb6837e
Khant Kyaw Khaung <khantkyawkhaung288@gmail.com>
2021-10-24 14:51:24 +0630

    :card_file_box: Web resource

--- charlotte/environment.py
+++ charlotte/environment.py
@@ -2,6 +2,6 @@ import os
 
 app_path = os.path.dirname(os.path.realpath(__file__))
 owner = '<@000000000000000000>'
-resource_url = 'https://charlotte3-bdo-web.herokuapp.com/static'
+resource_url = 'https://cdn.statically.io/gh/khantkyawkhaung/charlotte/main/web'
 tmp = '../tmp'
 token = ''
